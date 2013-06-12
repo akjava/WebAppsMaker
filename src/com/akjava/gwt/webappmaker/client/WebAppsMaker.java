74edf614556c1893d378ac84043a0f24c7c95a98
aki <aki@xps>
2013-06-12 13:24:49 +0900

    initial

---
+++ src/com/akjava/gwt/webappmaker/client/WebAppsMaker.java
@@ -0,0 +1,12 @@
+package com.akjava.gwt.webappmaker.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class WebAppsMaker implements EntryPoint {
+
+	public void onModuleLoad() {
+	}
+}
